a9e79ec7a704cd8613f8164b704e8774b4a22b05
Rory Trunkhill <rtrunkhill@gmail.com>
2017-08-21 22:27:10 +0000

    cp6 assignment complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -85,5 +85,35 @@
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
      end
    end
+      
+   describe "#import from second_csv" do
+     it "imports the correct number of entries from second csv" do
+       book.import_from_csv("entries_2.csv")
+       book_size = book.entries.size
  
+       expect(book_size).to eq 3
+     end
+     
+     
+     it "imports the 1st entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_one = book.entries[0]
+       check_entry(entry_one, "Dewey", "222-222-2222", "dewey@blocmail.com")
+     end
+     
+     
+     it "imports the 2rd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Huey", "555-555-1111", "huey@blocmail.com")
+     end
+     
+     
+    it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Luey", "555-555-3333", "luey@blocmail.com")
+     end
+     
+    end
  end
